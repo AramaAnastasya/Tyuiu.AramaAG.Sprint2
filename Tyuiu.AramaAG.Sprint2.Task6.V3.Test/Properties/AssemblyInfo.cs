42738b373445d00148e776dd396a3bfc8a153834
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-09 16:43:14 +0500

    Task6

---
+++ Tyuiu.AramaAG.Sprint2.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AramaAG.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AramaAG.Sprint2.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed6dfd7a-89a4-4667-9bb7-aa1112e6b2e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AramaAG.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint2.Task6.V3.Lib;
+
+namespace Tyuiu.AramaAG.Sprint2.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Составить программу, которая в зависимости от порядкового номера        *");
+            Console.WriteLine("* дня недели (1, 2, …, 7) выводит на экран его название.                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value;
+            Console.WriteLine("Введите порядковый номер дня недели:");
+            value = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if (value < 1 || value > 7)
+            {
+                res = $"Порядковый номер дня недели должен быть от 1 до 7. Значение {value}";
+            }
+            else
+            {
+                res = ds.FindDayName(value);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
