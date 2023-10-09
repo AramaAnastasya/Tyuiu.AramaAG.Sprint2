cbae709e483a961ab681b47ff44b7062582015bd
AramaAnastasya <Tyuiu.AramaAG@outlook.com>
2023-10-09 15:17:01 +0500

    Task5

---
+++ Tyuiu.AramaAG.Sprint2.Task5.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AramaAG.Sprint2.Task5.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AramaAG.Sprint2.Task5.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb0d4c01-9639-45aa-8972-0253d1fe988b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AramaAG.Sprint2.Task5.V4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.AramaAG.Sprint2.Task5.V4.Lib;
+
+namespace Tyuiu.AramaAG.Sprint2.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Арама А.Г. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Оператор switch                                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Арама Анастасия Георгиевна | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Мастям игральных карт условно присвоены следующие порядковые номера:    *");
+            Console.WriteLine("* масти «пики»—1, масти «трефы»—2, масти «бубны» — 3, масти «червы» — 4.  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value;
+            Console.WriteLine("Введите порядковый масти карты:");
+            value = Convert.ToInt32(Console.ReadLine());
+            string res;
+            if (value < 1 || value > 4)
+            {
+                res = $"Порядковый номер масти должен быть от 1 до 4. Значение {value}";
+            }
+            else
+            {
+                res = ds.FindCardSuit(value);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
